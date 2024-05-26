140586474386e0293ba8fe91bf78104a011e47b9
谢诗杜 <2211045@mail.dhu.edu.cn>
2024-05-26 20:26:52 +0800

    代码开发模块划分开发规范

--- service/server/qywx.py
+++ service/server/qywx.py
@@ -4,9 +4,9 @@ from service.utils.CheckURL import checkURL
 qywx = APIRouter()
 
 
-@qywx.get("/qywx")
-def vertify(request: Request):
-    print(checkURL(request))
+@qywx.get("/qywx", summary="企业微信的验证接口", description="用于验证服务器有效性",
+          response_description= "约定的验证密钥",)
+async def vertify(request: Request):
     return checkURL(request)
 
 
