7ab8e342124cf7d23996ada00da761737562fc6f
谢诗杜 <2211045@mail.dhu.edu.cn>
2024-05-26 19:02:21 +0800

    代码开发模块划分

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+# 模块1：消息生成模块
+# 模块2：消息管理模块（接收和回复）
+# 模块3：API模块
+# 模块4：日志模块
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 知识库存储
+# api-key存储
+# 临时文件存储
