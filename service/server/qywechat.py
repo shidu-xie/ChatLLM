7bc9cfd46277a46358432af5601bac215adcba7b
谢诗杜 <2211045@mail.dhu.edu.cn>
2024-05-29 01:06:53 +0800

    接收企业微信post请求，确定请求体参数类型

--- service/server/qywechat.py
+++ service/server/qywechat.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Body
 from service.utils.CheckURL import checkURL
 from service.utils.WXBizMsgCrypt3 import WXBizMsgCrypt
 from pydantic import BaseModel
@@ -7,7 +7,7 @@ from config.qywx import TOKEN, ENCODINGAESKEY, CROP_ID
 qywechat = APIRouter()
 
 class Item(BaseModel):
-    data: str
+    data: bytes
 
 @qywechat.get("/qywx", summary="企业微信的验证接口", description="用于验证服务器有效性",
           response_description= "约定的验证密钥",)
@@ -16,10 +16,18 @@ async def vertify(msg_signature: str, timestamp: str, nonce: str, echostr: str):
     return checkURL(msg_signature, timestamp, nonce, echostr)
 
 @qywechat.post("/qywx")
-async def reply(msg_signature: str, timestamp: str, nonce: str, item: Item):
+async def reply(msg_signature: str, timestamp: str, nonce: str, data: bytes = Body()):
+   data = data.decode('utf-8')
+   ret, sMsg = WXBizMsgCrypt(TOKEN, ENCODINGAESKEY, CROP_ID).DecryptMsg(data,msg_signature,timestamp, nonce)
+   return {"shop": "bed"}
 
-    data = item.data.encode('utf-8')
-    print(data)
-    ret, sMsg = WXBizMsgCrypt(TOKEN, ENCODINGAESKEY, CROP_ID).DecryptMsg(data,msg_signature,timestamp, nonce)
 
-    return {"shop": "bed"}
+# 该函数来查看请求的内容
+# async def get_headers(request: Request):
+#     headers = request.headers
+#     body = await request.body()
+#     # 在这里可以对请求体进行处理
+#     print(f"请求体内容：{body}")
+#     print(type(body))
+#     print(request.body())
+#     return headers
